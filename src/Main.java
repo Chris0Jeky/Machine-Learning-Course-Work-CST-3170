a170bf983597dfe18621fcdafde7c63ba56cd5ff
Chris <59696583+Chris0Jeky@users.noreply.github.com>
2024-10-09 15:39:01 +0100

    importing more code from labs

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
     // Constants for dataset dimensions
@@ -20,6 +22,11 @@ public class Main {
 
         // Optionally, print the dataset to verify correctness
         print2DArr(dataSet1);
+
+        int[] numbers = {1, 2, 3, 4};
+
+        printArray(numbers);
+        printArraysInHashSet(sideEffectSwappingCombinations(numbers));
     }
 
     // Function to initialize the 2D array by reading the CSV file
@@ -69,6 +76,57 @@ public class Main {
                 System.out.print(anInt + " ");
             }
             System.out.println(); // Move to the next line after printing a row
+        }
+    }
+
+    public static Set<int[]> sideEffectSwappingCombinations(int[] numbers){
+        Set<int[]> sideEffectedCombinations = new HashSet<>();
+        Set<int[]> permutations = new HashSet<>();
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < numbers.length; j++) {
+                sideEffectedCombinations.add(numbers);
+                permutations.add(numbers);
+                if (i != j) swap(numbers, i, j);
+            }
+        }
+
+        return pureSwappingCombinations(sideEffectedCombinations, permutations);
+    }
+
+    public static Set<int[]> pureSwappingCombinations(Set<int[]> sideEffectedCombinations, Set<int[]> permutations){
+
+        for (int[] combination: sideEffectedCombinations) {
+            for (int i = 0; i < combination.length; i++) {
+                for (int j = 0; j < combination.length; j++) {
+                    int[] temporaryCombination = combination.clone();
+                    permutations.add(swap(temporaryCombination, i, j));
+                }
+            }
+        }
+
+        return permutations;
+    }
+
+    public static int[] swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+
+        return arr;
+    }
+
+    public static void printArray(int[] arr){
+        for (int element : arr) {
+            System.out.println(element);
+        }
+    }
+
+    public static void printArraysInHashSet(Set<int[]> set){
+        int i = 0;
+        for (int[] array: set) {
+            i++;
+            System.out.println("Combination: " + i);
+            printArray(array);
         }
     }
 }
