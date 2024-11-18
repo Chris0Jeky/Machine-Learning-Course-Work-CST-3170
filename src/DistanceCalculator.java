6ef43860532b4d190d83dc2d4a5617f0cac2402e
Chris0Jeky <jeky.tck@gmail.com>
2024-11-18 01:52:26 +0000

    Update DistanceCalculator.java

--- src/DistanceCalculator.java
+++ src/DistanceCalculator.java
@@ -1,2 +1,10 @@
 public class DistanceCalculator {
+    public static double euclideanDistance(int[] vector1, int[] vector2) {
+        double sum = 0.0;
+        for (int i = 0; i < vector1.length; i++) {
+            double diff = vector1[i] - vector2[i];
+            sum += diff * diff;
+        }
+        return Math.sqrt(sum);
+    }
 }
