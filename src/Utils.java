ee36224c40bf176e1c669c0c3051b1c1ff2fba29
Chris0Jeky <jeky.tck@gmail.com>
2024-11-18 01:52:12 +0000

    implemented utils

--- src/Utils.java
+++ src/Utils.java
@@ -1,2 +1,19 @@
 public class Utils {
+    // Extract features (first 64 columns)
+    public static int[][] extractFeatures(int[][] data) {
+        int[][] features = new int[data.length][64];
+        for (int i = 0; i < data.length; i++) {
+            System.arraycopy(data[i], 0, features[i], 0, 64);
+        }
+        return features;
+    }
+
+    // Extract labels (last column)
+    public static int[] extractLabels(int[][] data) {
+        int[] labels = new int[data.length];
+        for (int i = 0; i < data.length; i++) {
+            labels[i] = data[i][64];
+        }
+        return labels;
+    }
 }
