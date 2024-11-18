ee7d938e1d6b90fbca1c4de36ecca20939ec4e52
Chris0Jeky <jeky.tck@gmail.com>
2024-11-18 02:53:43 +0000

    WIP

--- src/Utils.java
+++ src/Utils.java
@@ -1,3 +1,6 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class Utils {
     // Extract features (first 64 columns)
     public static int[][] extractFeatures(int[][] data) {
@@ -15,5 +18,31 @@ public class Utils {
             labels[i] = data[i][64];
         }
         return labels;
+    }
+
+    // Filter features for binary classes
+    public static int[][] filterBinaryClasses(int[][] features, int[] labels, int class1, int class2) {
+        List<int[]> filteredFeatures = new ArrayList<>();
+        for (int i = 0; i < labels.length; i++) {
+            if (labels[i] == class1 || labels[i] == class2) {
+                filteredFeatures.add(features[i]);
+            }
+        }
+        return filteredFeatures.toArray(new int[0][]);
+    }
+
+    // Filter labels for binary classes
+    public static int[] filterBinaryLabels(int[] labels, int class1, int class2) {
+        List<Integer> filteredLabels = new ArrayList<>();
+        for (int label : labels) {
+            if (label == class1 || label == class2) {
+                filteredLabels.add(label);
+            }
+        }
+        int[] result = new int[filteredLabels.size()];
+        for (int i = 0; i < filteredLabels.size(); i++) {
+            result[i] = filteredLabels.get(i);
+        }
+        return result;
     }
 }
