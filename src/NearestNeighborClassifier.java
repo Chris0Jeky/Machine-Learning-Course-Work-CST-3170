15ab45f250326f061ec7b51345697a10048a2a64
Chris0Jeky <jeky.tck@gmail.com>
2024-11-18 01:52:35 +0000

    Update NearestNeighborClassifier.java

--- src/NearestNeighborClassifier.java
+++ src/NearestNeighborClassifier.java
@@ -1,2 +1,24 @@
-public class NearestNeighborClassifier {
+public class NearestNeighborClassifier implements Classifier {
+    private int[][] trainingFeatures;
+    private int[] trainingLabels;
+
+    public NearestNeighborClassifier(int[][] trainingFeatures, int[] trainingLabels) {
+        this.trainingFeatures = trainingFeatures;
+        this.trainingLabels = trainingLabels;
+    }
+
+    @Override
+    public int predict(int[] testImage) {
+        double minDistance = Double.MAX_VALUE;
+        int predictedLabel = -1;
+
+        for (int i = 0; i < trainingFeatures.length; i++) {
+            double distance = DistanceCalculator.euclideanDistance(testImage, trainingFeatures[i]);
+            if (distance < minDistance) {
+                minDistance = distance;
+                predictedLabel = trainingLabels[i];
+            }
+        }
+        return predictedLabel;
+    }
 }
