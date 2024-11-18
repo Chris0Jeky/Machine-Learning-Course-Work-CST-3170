18637797d149304654f052e9b9aeada05c832570
Chris0Jeky <jeky.tck@gmail.com>
2024-11-18 02:58:47 +0000

    removed code that deals with binarisation

--- src/Utils.java
+++ src/Utils.java
@@ -20,32 +20,6 @@ public class Utils {
         return labels;
     }
 
-    // Filter features for binary classes
-    public static int[][] filterBinaryClasses(int[][] features, int[] labels, int class1, int class2) {
-        List<int[]> filteredFeatures = new ArrayList<>();
-        for (int i = 0; i < labels.length; i++) {
-            if (labels[i] == class1 || labels[i] == class2) {
-                filteredFeatures.add(features[i]);
-            }
-        }
-        return filteredFeatures.toArray(new int[0][]);
-    }
-
-    // Filter labels for binary classes
-    public static int[] filterBinaryLabels(int[] labels, int class1, int class2) {
-        List<Integer> filteredLabels = new ArrayList<>();
-        for (int label : labels) {
-            if (label == class1 || label == class2) {
-                filteredLabels.add(label);
-            }
-        }
-        int[] result = new int[filteredLabels.size()];
-        for (int i = 0; i < filteredLabels.size(); i++) {
-            result[i] = filteredLabels.get(i);
-        }
-        return result;
-    }
-
     public static int getMaxLabel(int[] labels) {
         int max = Integer.MIN_VALUE;
         for (int label : labels) {
@@ -55,5 +29,4 @@ public class Utils {
         }
         return max;
     }
-
 }
