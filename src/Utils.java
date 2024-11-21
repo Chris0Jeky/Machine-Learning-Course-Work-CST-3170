c7cc5ceeb5ab358ee9b6e1da30c4c4aea6b6d4f8
Chris0Jeky <jeky.tck@gmail.com>
2024-11-21 15:48:34 +0000

    Update Utils.java

--- src/Utils.java
+++ src/Utils.java
@@ -1,5 +1,4 @@
-import java.util.List;
-import java.util.ArrayList;
+import java.util.Random;
 
 public class Utils {
     // Extract features (first 64 columns)
@@ -28,5 +27,47 @@ public class Utils {
             }
         }
         return max;
+    }
+
+    // Combine two datasets
+    public static int[][] combineDatasets(int[][] dataSet1, int[][] dataSet2) {
+        int totalLength = dataSet1.length + dataSet2.length;
+        int[][] combinedData = new int[totalLength][];
+        System.arraycopy(dataSet1, 0, combinedData, 0, dataSet1.length);
+        System.arraycopy(dataSet2, 0, combinedData, dataSet1.length, dataSet2.length);
+        return combinedData;
+    }
+
+    // Shuffle the dataset
+    public static void shuffleDataset(int[][] data) {
+        Random rand = new Random();
+        for (int i = data.length - 1; i > 0; i--) {
+            int j = rand.nextInt(i + 1);
+            int[] temp = data[i];
+            data[i] = data[j];
+            data[j] = temp;
+        }
+    }
+
+    // Split dataset into k folds
+    public static int[][][] splitIntoFolds(int[][] features, int[] labels, int k) {
+        int n = features.length;
+        int foldSize = n / k;
+        int[][][] folds = new int[k][2][]; // Each fold has [features][labels]
+
+        for (int fold = 0; fold < k; fold++) {
+            int start = fold * foldSize;
+            int end = (fold == k - 1) ? n : start + foldSize;
+
+            int[][] foldFeatures = new int[end - start][];
+            int[] foldLabels = new int[end - start];
+
+            System.arraycopy(features, start, foldFeatures, 0, end - start);
+            System.arraycopy(labels, start, foldLabels, 0, end - start);
+
+            folds[fold][0] = foldFeatures;
+            folds[fold][1] = foldLabels;
+        }
+        return folds;
     }
 }
