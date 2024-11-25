f5ffa1834da5f26b14c556e8677857e7608426ea
Chris <59696583+Chris0Jeky@users.noreply.github.com>
2024-11-25 14:15:43 +0000

    deleting unused Fold class

--- src/Fold.java
+++
@@ -1,9 +0,0 @@
-public class Fold {
-    public int[][] features;
-    public int[] labels;
-
-    public Fold(int[][] features, int[] labels) {
-        this.features = features;
-        this.labels = labels;
-    }
-}
