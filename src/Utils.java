b513244223fef577c4e56245b44fb723f28735a2
Chris <59696583+Chris0Jeky@users.noreply.github.com>
2024-11-25 12:40:38 +0000

    reverting from mixing datasets

--- src/Utils.java
+++ src/Utils.java
@@ -31,45 +31,19 @@ public class Utils {
         return max;
     }
 
-    // Combine two datasets
-    public static int[][] combineDatasets(int[][] dataSet1, int[][] dataSet2) {
-        int totalLength = dataSet1.length + dataSet2.length;
-        int[][] combinedData = new int[totalLength][];
-        System.arraycopy(dataSet1, 0, combinedData, 0, dataSet1.length);
-        System.arraycopy(dataSet2, 0, combinedData, dataSet1.length, dataSet2.length);
-        return combinedData;
-    }
-
-    // Shuffle the dataset
-    public static void shuffleDataset(int[][] data) {
-        Random rand = new Random();
-        for (int i = data.length - 1; i > 0; i--) {
-            int j = rand.nextInt(i + 1);
-            int[] temp = data[i];
-            data[i] = data[j];
-            data[j] = temp;
+    // Get maximum label from both label arrays
+    public static int getMaxLabel(int[] labels1, int[] labels2) {
+        int max = Integer.MIN_VALUE;
+        for (int label : labels1) {
+            if (label > max) {
+                max = label;
+            }
         }
-    }
-
-    // Split dataset into k folds
-    public static int[][][] splitIntoFolds(int[][] features, int[] labels, int k) {
-        int n = features.length;
-        int foldSize = n / k;
-        int[][][] folds = new int[k][2][]; // Each fold has [features][labels]
-
-        for (int fold = 0; fold < k; fold++) {
-            int start = fold * foldSize;
-            int end = (fold == k - 1) ? n : start + foldSize;
-
-            int[][] foldFeatures = new int[end - start][];
-            int[] foldLabels = new int[end - start];
-
-            System.arraycopy(features, start, foldFeatures, 0, end - start);
-            System.arraycopy(labels, start, foldLabels, 0, end - start);
-
-            folds[fold][0] = foldFeatures;
-            folds[fold][1] = foldLabels;
+        for (int label : labels2) {
+            if (label > max) {
+                max = label;
+            }
         }
-        return folds;
+        return max;
     }
 }
