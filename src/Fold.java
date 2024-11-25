517a17c95db666e24e7579c8f06af34b33b12c3c
Chris <59696583+Chris0Jeky@users.noreply.github.com>
2024-11-25 12:20:58 +0000

    Create Fold.java

---
+++ src/Fold.java
@@ -0,0 +1,9 @@
+public class Fold {
+    public int[][] features;
+    public int[] labels;
+
+    public Fold(int[][] features, int[] labels) {
+        this.features = features;
+        this.labels = labels;
+    }
+}
