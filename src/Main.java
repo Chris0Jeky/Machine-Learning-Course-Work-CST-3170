86e78a608522050e43189f046e705fba1803ba79
Chris <59696583+Chris0Jeky@users.noreply.github.com>
2024-11-25 12:41:23 +0000

    implemented successful 2-fold

--- src/Main.java
+++ src/Main.java
@@ -12,62 +12,76 @@ public class Main {
         int[][] dataSet2 = DataLoader.loadData(csvFileName2);
         System.out.println("Datasets loaded successfully!");
 
-        // Combine datasets
-        System.out.println("Combining datasets...");
-        int[][] combinedData = Utils.combineDatasets(dataSet1, dataSet2);
-        System.out.println("Datasets combined successfully!");
-
-// Shuffle combined dataset
-        System.out.println("Shuffling the combined dataset...");
-        Utils.shuffleDataset(combinedData);
-        System.out.println("Dataset shuffled successfully!");
-
-// Split into features and labels
-        int[][] features = Utils.extractFeatures(combinedData);
-        int[] labels = Utils.extractLabels(combinedData);
-
-// Split into two folds
-        System.out.println("Splitting dataset into two folds...");
-        int[][][] folds = Utils.splitIntoFolds(features, labels, 2); // 2 folds
-        System.out.println("Dataset split into folds successfully!");
-
-
-        // Prepare features and labels
-        int[][] trainingFeatures = Utils.extractFeatures(dataSet1);
-        int[] trainingLabels = Utils.extractLabels(dataSet1);
-        int[][] testFeatures = Utils.extractFeatures(dataSet2);
-        int[] testLabels = Utils.extractLabels(dataSet2);
+        // Prepare features and labels for both datasets
+        int[][] features1 = Utils.extractFeatures(dataSet1);
+        int[] labels1 = Utils.extractLabels(dataSet1);
+        int[][] features2 = Utils.extractFeatures(dataSet2);
+        int[] labels2 = Utils.extractLabels(dataSet2);
 
         // Number of classes
-        int numClasses = Utils.getMaxLabel(trainingLabels) + 1; // Assuming labels start from 0
+        int numClasses = Utils.getMaxLabel(labels1, labels2) + 1; // Assuming labels start from 0
 
         // Initialize classifiers
         System.out.println("Initializing classifiers...");
-        MulticlassSVMClassifier svm = new MulticlassSVMClassifier(0.001, 0.01, 1000, trainingFeatures[0].length, numClasses);
-        MulticlassPerceptronClassifier perceptron = new MulticlassPerceptronClassifier(1000, trainingFeatures[0].length, numClasses);
-        KNearestNeighborsClassifier knn = new KNearestNeighborsClassifier(3, numClasses); // k = 3
-        NearestNeighborClassifier nearestNeighbor = new NearestNeighborClassifier();
-
-        // Train and evaluate classifiers
-        Classifier[] classifiers = {svm, perceptron, knn, nearestNeighbor};
+        Classifier[] classifiers = {
+                new MulticlassSVMClassifier(0.001, 0.01, 1000, features1[0].length, numClasses),
+                new MulticlassPerceptronClassifier(1000, features1[0].length, numClasses),
+                new KNearestNeighborsClassifier(3, numClasses), // k = 3
+                new NearestNeighborClassifier()
+        };
         String[] classifierNames = {"Multiclass SVM", "Multiclass Perceptron", "k-NN", "Nearest Neighbor"};
 
-        for (int i = 0; i < classifiers.length; i++) {
-            System.out.println("\nTraining " + classifierNames[i] + "...");
-            classifiers[i].train(trainingFeatures, trainingLabels);
+        // Arrays to store accuracies
+        double[][] accuracies = new double[classifiers.length][2]; // [classifier][fold]
 
-            System.out.println("Evaluating " + classifierNames[i] + "...");
-            int correctPredictions = 0;
-            for (int j = 0; j < testFeatures.length; j++) {
-                int predictedLabel = classifiers[i].predict(testFeatures[j]);
-                if (predictedLabel == testLabels[j]) {
-                    correctPredictions++;
-                }
+        // Two-fold testing
+        for (int fold = 0; fold < 2; fold++) {
+            System.out.println("\n=== Fold " + (fold + 1) + " ===");
+
+            // Prepare training and testing data
+            int[][] trainFeatures, testFeatures;
+            int[] trainLabels, testLabels;
+
+            if (fold == 0) {
+                // First fold: Train on dataSet1, test on dataSet2
+                trainFeatures = features1;
+                trainLabels = labels1;
+                testFeatures = features2;
+                testLabels = labels2;
+            } else {
+                // Second fold: Train on dataSet2, test on dataSet1
+                trainFeatures = features2;
+                trainLabels = labels2;
+                testFeatures = features1;
+                testLabels = labels1;
             }
-            double accuracy = (double) correctPredictions / testFeatures.length * 100;
-            System.out.println(classifierNames[i] + " Accuracy: " + accuracy + "%");
+
+            // For each classifier, train and evaluate
+            for (int i = 0; i < classifiers.length; i++) {
+                System.out.println("\nTraining " + classifierNames[i] + "...");
+                classifiers[i].train(trainFeatures, trainLabels);
+
+                System.out.println("Evaluating " + classifierNames[i] + "...");
+                int correctPredictions = 0;
+                for (int j = 0; j < testFeatures.length; j++) {
+                    int predictedLabel = classifiers[i].predict(testFeatures[j]);
+                    if (predictedLabel == testLabels[j]) {
+                        correctPredictions++;
+                    }
+                }
+                double accuracy = (double) correctPredictions / testFeatures.length * 100;
+                accuracies[i][fold] = accuracy;
+                System.out.println(classifierNames[i] + " Accuracy: " + accuracy + "%");
+            }
         }
 
-        System.out.println("All classifiers evaluated successfully!");
+        // Calculate and display average accuracies
+        System.out.println("\n=== Average Accuracies ===");
+        for (int i = 0; i < classifiers.length; i++) {
+            double averageAccuracy = (accuracies[i][0] + accuracies[i][1]) / 2;
+            System.out.println(classifierNames[i] + " Average Accuracy: " + averageAccuracy + "%");
+        }
+
+        System.out.println("Two-fold testing completed successfully!");
     }
 }
