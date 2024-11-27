79df0bd3b4e4532e42836810d5ca65521a1f049e
Chris <59696583+Chris0Jeky@users.noreply.github.com>
2024-11-27 14:44:22 +0000

    Update LinearKernel.java

--- src/LinearKernel.java
+++ src/LinearKernel.java
@@ -1,2 +1,10 @@
-public class LinearKernel {
+public class LinearKernel implements Kernel {
+    @Override
+    public double compute(int[] x1, int[] x2) {
+        double result = 0.0;
+        for (int i = 0; i < x1.length; i++) {
+            result += x1[i] * x2[i];
+        }
+        return result;
+    }
 }
