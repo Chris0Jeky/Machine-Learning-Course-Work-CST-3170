a96c02caa95be39796e56de4db06f8337545fdff
Chris <59696583+Chris0Jeky@users.noreply.github.com>
2024-11-27 14:45:29 +0000

    Update PolynomialKernel.java

--- src/PolynomialKernel.java
+++ src/PolynomialKernel.java
@@ -1,2 +1,20 @@
-public class PolynomialKernel {
+public class PolynomialKernel implements Kernel {
+    private int degree;
+    private double coef0;
+    private double gamma;
+
+    public PolynomialKernel(int degree, double coef0, double gamma) {
+        this.degree = degree;
+        this.coef0 = coef0;
+        this.gamma = gamma;
+    }
+
+    @Override
+    public double compute(int[] x1, int[] x2) {
+        double dotProduct = 0.0;
+        for (int i = 0; i < x1.length; i++) {
+            dotProduct += x1[i] * x2[i];
+        }
+        return Math.pow(gamma * dotProduct + coef0, degree);
+    }
 }
