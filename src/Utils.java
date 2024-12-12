7e9eacce5b34504c774be1049113b76103cd1de5
Chris0Jeky <jeky.tck@gmail.com>
2024-12-12 21:06:21 +0000

    added deepCopy function

--- src/Utils.java
+++ src/Utils.java
@@ -144,4 +144,13 @@ public class Utils {
         }
         return Math.sqrt(sum);
     }
+
+    public static int[][] deepCopy(int[][] original) {
+        int[][] copy = new int[original.length][];
+        for (int i = 0; i < original.length; i++) {
+            copy[i] = Arrays.copyOf(original[i], original[i].length);
+        }
+        return copy;
+    }
+
 }
