a7664ff526de2b851849e41b8150bdb54cd54177
Chris0Jeky <jeky.tck@gmail.com>
2024-12-13 04:44:22 +0000

    comments

--- src/Classifier.java
+++ src/Classifier.java
@@ -1,4 +1,6 @@
 public interface Classifier {
+    // A generic interface for classifiers.
+    // Implementations must provide a training method and a prediction method.
     void train(int[][] features, int[] labels);
     int predict(int[] sample);
 }
