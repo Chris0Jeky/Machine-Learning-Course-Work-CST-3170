5de9a0b90b8b3d56d7c27491e2270b607d59c3a3
Chris0Jeky <jeky.tck@gmail.com>
2024-12-13 04:44:35 +0000

    comments

--- src/DistanceCalculator.java
+++ src/DistanceCalculator.java
@@ -1,4 +1,5 @@
 public class DistanceCalculator {
+    // Computes Euclidean distance between two feature vectors
     public static double euclideanDistance(int[] vector1, int[] vector2) {
         double sum = 0.0;
         for (int i = 0; i < vector1.length; i++) {
