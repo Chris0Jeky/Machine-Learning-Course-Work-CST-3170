77decd4e37c1b02d001c5cbb63ff92d3b87a1f64
Chris0Jeky <jeky.tck@gmail.com>
2024-12-13 04:53:55 +0000

    comments

--- src/Kernel.java
+++ src/Kernel.java
@@ -1,3 +1,5 @@
 public interface Kernel {
+    // Represents a kernel function used by kernel-based SVMs.
+    // Given two feature vectors x1 and x2, compute returns their kernelized similarity.
     double compute(int[] x1, int[] x2);
 }
