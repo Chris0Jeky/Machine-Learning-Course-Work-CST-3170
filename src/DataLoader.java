c3bb5c6576f78825e644fd6aaa5ebcde8dc01125
Chris0Jeky <jeky.tck@gmail.com>
2024-12-13 04:44:30 +0000

    comments

--- src/DataLoader.java
+++ src/DataLoader.java
@@ -7,29 +7,27 @@ import java.util.Scanner;
 public class DataLoader {
     private static final String COMMA_DELIMITER = ",";
 
+    // Loads a CSV file of integers into a 2D array.
+    // Each row of the CSV is converted to an int[].
+    // Any parsing errors yield a default value of 0 for that cell.
     public static int[][] loadData(String csvFileName) {
         List<int[]> dataList = new ArrayList<>();
-
-        // Create a File object
         File file = new File(csvFileName);
         System.out.println("Attempting to open file at path: " + file.getAbsolutePath());
 
         try (Scanner scanner = new Scanner(file)) {
-            // Read each line from the file
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
                 String[] values = line.split(COMMA_DELIMITER);
-
                 int[] row = new int[values.length];
                 for (int col = 0; col < values.length; col++) {
                     try {
                         row[col] = Integer.parseInt(values[col]);
                     } catch (NumberFormatException e) {
-                        System.out.println("Error: Invalid number format at row " + (dataList.size() + 1) + ", column " + (col + 1));
-                        row[col] = 0; // Assign a default value (0) in case of invalid format
+                        System.out.println("Error parsing value at row " + (dataList.size() + 1) + ", column " + (col + 1));
+                        row[col] = 0;
                     }
                 }
-
                 dataList.add(row);
             }
         } catch (FileNotFoundException e) {
@@ -38,7 +36,6 @@ public class DataLoader {
             return new int[0][];
         }
 
-        // Convert the list to a 2D array
         return dataList.toArray(new int[0][]);
     }
 }
